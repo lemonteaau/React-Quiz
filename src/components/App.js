868a6676c394900e459743ba9b612b317978bc01
Terry Cheng <cyr062599@gmail.com>
2024-04-21 17:03:09 +0930

    Add answer prop to Question component and update Progress component

--- src/components/App.js
+++ src/components/App.js
@@ -91,6 +91,7 @@ export default function App() {
               numQuestions={numQuestions}
               points={points}
               maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
             />
             <Question
               question={questions[index]}
