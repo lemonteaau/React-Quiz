c3de8836d3be4ac80945391fcc06f400f5d0c30d
Terry Cheng <cyr062599@gmail.com>
2024-04-21 15:57:27 +0930

    Refactor App.js to handle data fetching and state management

--- src/App.js
+++ src/App.js
@@ -2,14 +2,40 @@ import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+
+  //'loading','error','ready','active','finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
   const [state, dispath] = useReducer(reducer, initialState);
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log(err));
+      .then((data) => dispath({ type: "dataReceived", payload: data }))
+      .catch((err) => dispath({ type: "dataFailed" }));
   }, []);
 
   return (
