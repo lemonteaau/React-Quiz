793483ecc227fd6f6c2d87b4a6d3eedb7c547c6b
Terry Cheng <cyr062599@gmail.com>
2024-04-21 17:29:07 +0930

    Add restart functionality

--- src/components/App.js
+++ src/components/App.js
@@ -57,6 +57,12 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
     default:
       throw new Error("Action unknown");
   }
@@ -113,6 +119,7 @@ export default function App() {
         )}
         {status === "finished" && (
           <FinishScreen
+            dispatch={dispatch}
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
