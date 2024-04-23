a22cefcfab189be65a349ec87771c8fbceb20e03
Terry Cheng <cyr062599@gmail.com>
2024-04-23 16:59:06 +0930

    Refactor by using ContextAPI

--- src-no-context/index.js
+++ src-no-context/index.js
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import { QuizProvider } from "./components/context/QuizProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <QuizProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </QuizProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
